540b8c6f22e9bdf2a3d61ffef25d0512946400a5
Merlin GUYARD <merlin.guyard@gmail.com>
2021-12-14 01:44:15 +0100

    Init

---
+++ Warning/src/test/java/com/MG/Warning/WarningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MG.Warning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
