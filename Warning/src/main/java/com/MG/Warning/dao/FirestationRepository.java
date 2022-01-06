696494eed53d430d1a222b68d297716efaddddad
Merlin GUYARD <merlin.guyard@gmail.com>
2022-01-06 21:04:58 +0100

    Firestationcontroller début

--- Warning/src/main/java/com/MG/Warning/dao/FirestationRepository.java
+++ Warning/src/main/java/com/MG/Warning/dao/FirestationRepository.java
@@ -2,6 +2,7 @@ package com.mg.warning.dao;
 
 
 import com.mg.warning.model.Firestation;
+import com.mg.warning.model.MedicalRecord;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -12,11 +13,29 @@ public class FirestationRepository {
 
     private List<Firestation> firestationList = new ArrayList<>();
 
+    //GET
+    public List<Firestation> findAll() {
+        return firestationList;
+    }
+
+//    //PUT
+//    public void update(Firestation firestation) {
+//        int index = 0;
+//        for (Firestation f : firestationList) {
+//            if (f.getFirstName().equals(medicalRecord.getFirstName()) && f.getLastName().equals(medicalRecord.getLastName())) {
+//                medicalRecordList.set(index, medicalRecord);
+//                index++;
+//            }
+//        }
+//    }
+
+    //POST
     public void save(Firestation firestation) {
         firestationList.add(firestation);
     }
 
-    public List<Firestation> findAll() {
-        return firestationList;
-    }
+//    //DELETE
+//    public void delete(String firstname, String lastname) {
+//        medicalRecordList.removeIf(m -> m.getFirstName().equals(firstname) && m.getLastName().equals(lastname));
+//    }
 }
