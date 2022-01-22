d1ca760c65568b8282617c761148e078a2db0e6f
Merlin GUYARD <merlin.guyard@gmail.com>
2022-01-22 21:21:22 +0100

    Child alert commented

--- Warning/src/main/java/com/mg/warning/alert/AlertController.java
+++ Warning/src/main/java/com/mg/warning/alert/AlertController.java
@@ -58,48 +58,48 @@ public class AlertController {
     public List<ChildrenWithFamilyDTO> getAllChildrenAndFamily(@RequestParam("address") String address) {
 //        FireStationAlertFindPersonsAndNbService fireStationAlertFindPersonsAndNbService = new FireStationAlertFindPersonsAndNbService();
 //        return fireStationAlertFindPersonsAndNbService.getFirestationAlertDTOWithSum(stationNumber);
-        String childAddress = null;
-        List<MedicalRecord> medicalRecords = medicalRecordRepository.findAll();
-        List<Person> persons = personRepository.findAll();
-        ChildrenWithFamilyDTO dtoChild = new ChildrenWithFamilyDTO();
-        List<ChildrenWithFamilyDTO> dtoChildList = new ArrayList<>();
-        FamilyDTO dtoFamily = new FamilyDTO();
-
-
-        LocalDate dateFrom18YearsAgo = LocalDate.now().minusYears(18);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
-        LocalDate personAge;
-        for (MedicalRecord medicalRecord : medicalRecords) {
-            personAge = LocalDate.parse(medicalRecord.getBirthdate(), formatter);
-            if (personAge.isAfter(dateFrom18YearsAgo)) {
-                dtoChild.setFirstname(medicalRecord.getFirstName());
-                dtoChild.setLastname(medicalRecord.getLastName());
-
-
-                for (Person person1 : persons) {
-                    if (dtoChild.getFirstname().equals(person1.getFirstName()) && dtoChild.getLastname().equals(person1.getLastName())) {
-                        childAddress = person1.getAddress();
-                    }
-
-                    List<FamilyDTO> dtoWithFamilyList = new ArrayList<>();
-                    for (Person person2 : persons) {
-                        if (person2.getAddress().equals(childAddress)) {
-                            dtoFamily.setFirstname(person2.getFirstName());
-                            dtoFamily.setLastname(person2.getLastName());
-                            dtoWithFamilyList.add(dtoFamily);
-                        }
-
-
-                        dtoChild.setAge(Period.between(personAge, LocalDate.now()).getYears());
-                        dtoChild.setFamily(dtoWithFamilyList);
-                        dtoChildList.add(dtoChild);
-                    }
-                }
-
-
-            }
-
-        }
-        return dtoChildList;
+//        String childAddress = null;
+//        List<MedicalRecord> medicalRecords = medicalRecordRepository.findAll();
+//        List<Person> persons = personRepository.findAll();
+//        ChildrenWithFamilyDTO dtoChild = new ChildrenWithFamilyDTO();
+//        List<ChildrenWithFamilyDTO> dtoChildList = new ArrayList<>();
+//        FamilyDTO dtoFamily = new FamilyDTO();
+//
+//
+//        LocalDate dateFrom18YearsAgo = LocalDate.now().minusYears(18);
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+//        LocalDate personAge;
+//        for (MedicalRecord medicalRecord : medicalRecords) {
+//            personAge = LocalDate.parse(medicalRecord.getBirthdate(), formatter);
+//            if (personAge.isAfter(dateFrom18YearsAgo)) {
+//                dtoChild.setFirstname(medicalRecord.getFirstName());
+//                dtoChild.setLastname(medicalRecord.getLastName());
+//
+//
+//                for (Person person1 : persons) {
+//                    if (dtoChild.getFirstname().equals(person1.getFirstName()) && dtoChild.getLastname().equals(person1.getLastName())) {
+//                        childAddress = person1.getAddress();
+//                    }
+//
+//                    List<FamilyDTO> dtoWithFamilyList = new ArrayList<>();
+//                    for (Person person2 : persons) {
+//                        if (person2.getAddress().equals(childAddress)) {
+//                            dtoFamily.setFirstname(person2.getFirstName());
+//                            dtoFamily.setLastname(person2.getLastName());
+//                            dtoWithFamilyList.add(dtoFamily);
+//                        }
+//
+//
+//                        dtoChild.setAge(Period.between(personAge, LocalDate.now()).getYears());
+//                        dtoChild.setFamily(dtoWithFamilyList);
+//                        dtoChildList.add(dtoChild);
+//                    }
+//                }
+//
+//
+//            }
+//
+//        }
+        return null;
     }
 }
