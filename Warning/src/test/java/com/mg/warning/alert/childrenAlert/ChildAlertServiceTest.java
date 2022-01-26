9b94b3532705f673b0b929dbd8b33c9aa8218c36
Merlin GUYARD <merlin.guyard@gmail.com>
2022-01-26 17:17:35 +0100

    added Children test

--- Warning/src/test/java/com/mg/warning/alert/childrenAlert/ChildAlertServiceTest.java
+++ Warning/src/test/java/com/mg/warning/alert/childrenAlert/ChildAlertServiceTest.java
@@ -1,12 +1,64 @@
 package com.mg.warning.alert.childrenAlert;
 
+import com.mg.warning.alert.AlertService;
+import com.mg.warning.medicalRecord.MedicalRecord;
+import com.mg.warning.medicalRecord.MedicalRecordRepository;
+import com.mg.warning.person.Person;
+import com.mg.warning.person.PersonRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
 
-class ChildAlertServiceTest {
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class ChildAlertServiceTest {
+
+        @Mock
+        private PersonRepository personRepository = mock(PersonRepository.class);
+
+        @Mock
+        private MedicalRecordRepository medicalRecordRepository = mock(MedicalRecordRepository.class);
+
+        @Mock
+        private AlertService alertService = mock(AlertService.class);
+
+        @InjectMocks
+        private ChildAlertService service = new ChildAlertService();
 
     @Test
-    void getChildrenWithFamilyDTO() {
+    void Test1() {
+
+        List<MedicalRecord> medicalRecords = new ArrayList<>();
+        MedicalRecord medicalRecord = new MedicalRecord();
+        List<Person> persons = new ArrayList<>();
+        Person person = new Person();
+
+
+        medicalRecord.setFirstName("Tom");
+        medicalRecord.setLastName("Sawyer");
+        medicalRecord.setBirthdate("03/06/2005");
+        medicalRecords.add(medicalRecord);
+        when(medicalRecordRepository.findAll())
+                .thenReturn(medicalRecords);
+
+        person.setFirstName("Tom");
+        person.setLastName("Sawyer");
+        person.setAddress("Auvers");
+        persons.add(person);
+        when(personRepository.findAll())
+                .thenReturn(persons);
+
+        when(alertService.getAgeFromMedicalRecords(medicalRecords, person.getFirstName(), person.getLastName())).thenReturn(17);
+
+        ChildrenAlertWithFamilyDTO result = service.getChildrenWithFamilyDTO("Auvers");
+        assertThat(result.getChildren()).isEqualTo("Tom");
+
     }
 }
