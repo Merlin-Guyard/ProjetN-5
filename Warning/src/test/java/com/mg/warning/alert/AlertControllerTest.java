3ebb8f7a6b75ba2b94a9cf514d67b60194de6431
Merlin GUYARD <merlin.guyard@gmail.com>
2022-04-25 01:51:00 +0200

    added 3 last controller tests

--- Warning/src/test/java/com/mg/warning/alert/AlertControllerTest.java
+++ Warning/src/test/java/com/mg/warning/alert/AlertControllerTest.java
@@ -1,7 +1,5 @@
 package com.mg.warning.alert;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mg.warning.controller.AlertController;
 import com.mg.warning.dto.*;
@@ -15,13 +13,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(AlertController.class)
 class AlertControllerTest {
@@ -70,8 +69,21 @@ class AlertControllerTest {
 
         mockMvc.perform(get("/firestation?stationNumber=42"))
                 .andExpect(status().isOk())
+                .andExpect(jsonPath("$.firestationAlertDTOS.[0].firstname", is("Bobby")))
                 .andExpect(jsonPath("$.nbAdults", is(1)))
                 .andExpect(jsonPath("$.nbChildrens", is(2)));
+    }
+
+    @Test
+    void getFirestationEmptyTest() throws Exception {
+
+        FirestationWithNbDTO firestationWithNbDTO = new FirestationWithNbDTO();
+
+        when(firestationAlertService.getFirestationAlertDTOWithSum(42)).thenReturn(firestationWithNbDTO);
+
+        mockMvc.perform(get("/firestation?stationNumber=42"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.firestationAlertDTOS.[0].firstname").doesNotExist());
     }
 
     @Test
@@ -103,6 +115,18 @@ class AlertControllerTest {
     }
 
     @Test
+    void getChildEmptyTest() throws Exception {
+
+        ChildrenWithFamilyDTO childrenWithFamilyDTO = new ChildrenWithFamilyDTO();
+
+        when(childAlertService.getChildrenWithFamilyDTO("TestAddress")).thenReturn(childrenWithFamilyDTO);
+
+        mockMvc.perform(get("/childAlert?address=TestAddress"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.children.[0].firstname").doesNotExist());
+    }
+
+    @Test
     void getPhoneTest() throws Exception {
 
         List<PhoneDTO> phoneDTOS = new ArrayList<>();
@@ -116,6 +140,19 @@ class AlertControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.[0].phone", is("06 06 06 06 06")));
     }
+
+    @Test
+    void getPhoneEmptyTest() throws Exception {
+
+        List<PhoneDTO> phoneDTOS = new ArrayList<>();
+
+        when(phoneAlertService.getPhoneDTO(42)).thenReturn(phoneDTOS);
+
+        mockMvc.perform(get("/phoneAlert?firestation=42"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.[0].phone").doesNotExist());
+    }
+
 
     @Test
     void getFireTest() throws Exception {
@@ -143,27 +180,114 @@ class AlertControllerTest {
                 .andExpect(jsonPath("$.fireAlertStationDTO.[0].station", is(42)));
     }
 
+    @Test
+    void getFireEmptyTest() throws Exception {
+
+        List<FirePersonDTO> firePersonDTOS = new ArrayList<>();
+        List<FireFireStationDTO> fireFireStationDTOS = new ArrayList<>();
+
+        FireDTO fireDTO = new FireDTO();
+        fireDTO.setFireAlertPersonsDTO(firePersonDTOS);
+        fireDTO.setFireAlertStationDTO(fireFireStationDTOS);
+
+        when(fireAlertService.getFireDTO("TestAddress")).thenReturn(fireDTO);
+
+        mockMvc.perform(get("/fire?address=TestAddress"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.fireAlertPersonsDTO.[0].lastName").doesNotExist());
+    }
+
 
     @Test
     void getFloodTest() throws Exception {
 
         List<FloodPersonsDTO> floodPersonsDTOS = new ArrayList<>();
         FloodPersonsDTO floodPersonsDTO = new FloodPersonsDTO();
-        floodPersonsDTO.setLastName("Dupon");
+        floodPersonsDTO.setLastName("Dupont");
         floodPersonsDTO.setAge(16);
         floodPersonsDTOS.add(floodPersonsDTO);
 
+        List<FloodDTO> floodDTOS = new ArrayList<>();
         FloodDTO floodDTO = new FloodDTO();
         floodDTO.setAddress("TestAddress");
         floodDTO.setFloodAlertPersonsDTO(floodPersonsDTOS);
+        floodDTOS.add(floodDTO);
 
-        when(floodAlertService.getFloodDTO(int)).thenReturn(floodDTO);
+        when(floodAlertService.getFloodDTO(new int[]{42})).thenReturn(floodDTOS);
 
         mockMvc.perform(get("/flood/stations?stations=42"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.address", is("TestAddress")))
-                .andExpect(jsonPath("$.floodAlertPersonsDTO.[0].lastName", is("Dupont")));
+                .andExpect(jsonPath("$.[0].address", is("TestAddress")))
+                .andExpect(jsonPath("$.[0].floodAlertPersonsDTO.[0].lastName", is("Dupont")));
     }
+    @Test
+    void getFloodEmptyTest() throws Exception {
+
+        List<FloodDTO> floodDTOS = new ArrayList<>();
+
+        when(floodAlertService.getFloodDTO(new int[]{42})).thenReturn(floodDTOS);
+
+        mockMvc.perform(get("/flood/stations?stations=42"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.[0].address").doesNotExist());
+    }
+
+    @Test
+    void getPersonInfoTest() throws Exception {
+
+        List<PersonInfoDTO> personInfoDTOS = new ArrayList<>();
+        PersonInfoDTO personInfoDTO = new PersonInfoDTO();
+        personInfoDTO.setLastName("Dupont");
+        personInfoDTO.setAge(16);
+        personInfoDTOS.add(personInfoDTO);
+
+        when(personInfoAlertService.getPersonInfoDTO("Bobby", "Dupont")).thenReturn(personInfoDTOS);
+
+        mockMvc.perform(get("/personInfo?firstName=Bobby&lastName=Dupont"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.[0].lastName", is("Dupont")))
+                .andExpect(jsonPath("$.[0].age", is(16)));
+    }
+
+    @Test
+    void getPersonInfoEmptyTest() throws Exception {
+
+        List<PersonInfoDTO> personInfoDTOS = new ArrayList<>();
+
+        when(personInfoAlertService.getPersonInfoDTO("Bobby", "Dupont")).thenReturn(personInfoDTOS);
+
+        mockMvc.perform(get("/personInfo?firstName=Bobby&lastName=Dupont"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.[0].lastName").doesNotExist());
+    }
+
+    @Test
+    void getEmailTest() throws Exception {
+
+        List<EmailDTO> emailDTOS = new ArrayList<>();
+        EmailDTO emailDTO = new EmailDTO();
+        emailDTO.setEmail("Dupont@gmail.com");
+        emailDTOS.add(emailDTO);
+
+        when(emailAlertService.getEmailDTO("Paris")).thenReturn(emailDTOS);
+
+        mockMvc.perform(get("/communityEmail?city=Paris"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.[0].email", is("Dupont@gmail.com")));
+    }
+
+    @Test
+    void getEmailEmptyTest() throws Exception {
+
+        List<EmailDTO> emailDTOS = new ArrayList<>();
+
+        when(emailAlertService.getEmailDTO("Paris")).thenReturn(emailDTOS);
+
+        mockMvc.perform(get("/communityEmail?city=Paris"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.[0].email").doesNotExist());
+    }
+
 
     public static String asJsonString(final Object obj) {
         try {
