d795b0281d953c9fbf1484a4cebf1907b54ad8e8
Merlin GUYARD <merlin.guyard@gmail.com>
2022-04-25 01:04:08 +0200

    added 4 alert cotnroller test on 7

--- Warning/src/test/java/com/mg/warning/alert/AlertControllerTest.java
+++ Warning/src/test/java/com/mg/warning/alert/AlertControllerTest.java
@@ -1,121 +1,175 @@
-//package com.mg.warning.alert;
-//
-//import com.mg.warning.service.*;
-//import com.mg.warning.service.ChildAlertService;
-//import com.mg.warning.controller.AlertController;
-//import com.mg.warning.repository.FirestationRepository;
-//import com.mg.warning.repository.MedicalRecordRepository;
-//import com.mg.warning.repository.PersonRepository;
-//import org.junit.jupiter.api.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-//import org.springframework.boot.test.context.TestConfiguration;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.test.context.junit4.SpringRunner;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//
-//@RunWith(SpringRunner.class)
-//@WebMvcTest(controllers = AlertController.class)
-//class AlertControllerTest {
-//
-//    @TestConfiguration
-//    static class AdditionalConfig {
-//        @Bean
-//        public FirestationRepository firestationRepository() {
-//            return new FirestationRepository();
-//        }
-//
-//        @Bean
-//        public PersonRepository personRepository() {
-//            return new PersonRepository();
-//        }
-//
-//        @Bean
-//        public MedicalRecordRepository medicalRecordRepository() {
-//            return new MedicalRecordRepository();
-//        }
-//
-//        @Bean
-//        public FirestationAlertService fireStationService() {
-//            return new FirestationAlertService();
-//        }
-//
-//        @Bean
-//        public ChildAlertService childService() {
-//            return new ChildAlertService();
-//        }
-//
-//        @Bean
-//        public PhoneAlertService phoneService() {
-//            return new PhoneAlertService();
-//        }
-//
-//        @Bean
-//        public FireAlertService fireService() {
-//            return new FireAlertService();
-//        }
-//
-//        @Bean
-//        public FloodAlertService floodService() {
-//            return new FloodAlertService();
-//        }
-//
-//        @Bean
-//        public PersonInfoAlertService personInfoService() {
-//            return new PersonInfoAlertService();
-//        }
-//
-//        @Bean
-//        public EmailAlertService emailService() {
-//            return new EmailAlertService();
-//        }
-//    }
-//
-//    @Autowired
-//    private MockMvc mvc;
-//
-//    @Test
-//    public void testGetFireStation() throws Exception {
-//        mvc.perform(MockMvcRequestBuilders.get("/firestation?stationNumber=42"))
-//                .andExpect(status().isOk());
-//    }
-//
-//    @Test
-//    public void testGetChildAlert() throws Exception {
-//        mvc.perform(MockMvcRequestBuilders.get("/childAlert?address=TestAddress"))
-//                .andExpect(status().isOk());
-//    }
-//
-//    @Test
-//    public void testGetPhone() throws Exception {
-//        mvc.perform(MockMvcRequestBuilders.get("/phoneAlert?firestation=42"))
-//                .andExpect(status().isOk());
-//    }
-//
-//    @Test
-//    public void testGetFire() throws Exception {
-//        mvc.perform(MockMvcRequestBuilders.get("/fire?address=TestAddress"))
-//                .andExpect(status().isOk());
-//    }
-//
-//    @Test
-//    public void testGetFlood() throws Exception {
-//        mvc.perform(MockMvcRequestBuilders.get("/flood/stations?stations=42"))
-//                .andExpect(status().isOk());
-//    }
-//
-//    @Test
-//    public void testGetPersonInfo() throws Exception {
-//        mvc.perform(MockMvcRequestBuilders.get("/personInfo?firstName=TestFirstName&lastName=TestFirstName"))
-//                .andExpect(status().isOk());
-//    }
-//
-//    @Test
-//    public void testGetCity() throws Exception {
-//        mvc.perform(MockMvcRequestBuilders.get("/communityEmail?city=TestCity"))
-//                .andExpect(status().isOk());
-//    }
-//}
+package com.mg.warning.alert;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mg.warning.controller.AlertController;
+import com.mg.warning.dto.*;
+import com.mg.warning.model.Person;
+import com.mg.warning.service.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@WebMvcTest(AlertController.class)
+class AlertControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private FirestationAlertService firestationAlertService;
+
+    @MockBean
+    private ChildAlertService childAlertService;
+
+    @MockBean
+    private PhoneAlertService phoneAlertService;
+
+    @MockBean
+    private FireAlertService fireAlertService;
+
+    @MockBean
+    private FloodAlertService floodAlertService;
+
+    @MockBean
+    private PersonInfoAlertService personInfoAlertService;
+
+    @MockBean
+    private EmailAlertService emailAlertService;
+
+    @Test
+    void getFirestationTest() throws Exception {
+
+        List<FirestationDTO> firestationDTOS = new ArrayList<>();
+        FirestationDTO firestationDTO = new FirestationDTO();
+        firestationDTO.setFirstname("Bobby");
+        firestationDTO.setLastname("Dupont");
+        firestationDTO.setAddress("Strawberry road");
+        firestationDTO.setPhone("06 06 06 06 06");
+        firestationDTOS.add(firestationDTO);
+
+        FirestationWithNbDTO firestationWithNbDTO = new FirestationWithNbDTO();
+        firestationWithNbDTO.setFirestationAlertDTOS(firestationDTOS);
+        firestationWithNbDTO.setNbAdults(1);
+        firestationWithNbDTO.setNbChildrens(2);
+
+        when(firestationAlertService.getFirestationAlertDTOWithSum(42)).thenReturn(firestationWithNbDTO);
+
+        mockMvc.perform(get("/firestation?stationNumber=42"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.nbAdults", is(1)))
+                .andExpect(jsonPath("$.nbChildrens", is(2)));
+    }
+
+    @Test
+    void getChildTest() throws Exception {
+
+        List<ChildrenDTO> childrenDTOS = new ArrayList<>();
+        ChildrenDTO childrenDTO = new ChildrenDTO();
+        childrenDTO.setFirstname("Bobby");
+        childrenDTO.setLastname("Dupont");
+        childrenDTO.setAge(16);
+        childrenDTOS.add(childrenDTO);
+
+        List<FamilyDTO> familyDTOS = new ArrayList<>();
+        FamilyDTO familyDTO = new FamilyDTO();
+        familyDTO.setFirstname("Sarah");
+        familyDTO.setLastname("Dupont");
+        familyDTOS.add(familyDTO);
+
+        ChildrenWithFamilyDTO childrenWithFamilyDTO = new ChildrenWithFamilyDTO();
+        childrenWithFamilyDTO.setChildren(childrenDTOS);
+        childrenWithFamilyDTO.setFamily(familyDTOS);
+
+        when(childAlertService.getChildrenWithFamilyDTO("TestAddress")).thenReturn(childrenWithFamilyDTO);
+
+        mockMvc.perform(get("/childAlert?address=TestAddress"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.children.[0].firstname", is("Bobby")))
+                .andExpect(jsonPath("$.family.[0]firstname", is("Sarah")));
+    }
+
+    @Test
+    void getPhoneTest() throws Exception {
+
+        List<PhoneDTO> phoneDTOS = new ArrayList<>();
+        PhoneDTO phoneDTO = new PhoneDTO();
+        phoneDTO.setPhone("06 06 06 06 06");
+        phoneDTOS.add(phoneDTO);
+
+        when(phoneAlertService.getPhoneDTO(42)).thenReturn(phoneDTOS);
+
+        mockMvc.perform(get("/phoneAlert?firestation=42"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.[0].phone", is("06 06 06 06 06")));
+    }
+
+    @Test
+    void getFireTest() throws Exception {
+
+        List<FirePersonDTO> firePersonDTOS = new ArrayList<>();
+        FirePersonDTO firePersonDTO = new FirePersonDTO();
+        firePersonDTO.setLastName("Dupont");
+        firePersonDTO.setAge(16);
+        firePersonDTOS.add(firePersonDTO);
+
+        List<FireFireStationDTO> fireFireStationDTOS = new ArrayList<>();
+        FireFireStationDTO fireFireStationDTO = new FireFireStationDTO();
+        fireFireStationDTO.setStation(42);
+        fireFireStationDTOS.add(fireFireStationDTO);
+
+        FireDTO fireDTO = new FireDTO();
+        fireDTO.setFireAlertPersonsDTO(firePersonDTOS);
+        fireDTO.setFireAlertStationDTO(fireFireStationDTOS);
+
+        when(fireAlertService.getFireDTO("TestAddress")).thenReturn(fireDTO);
+
+        mockMvc.perform(get("/fire?address=TestAddress"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.fireAlertPersonsDTO.[0].lastName", is("Dupont")))
+                .andExpect(jsonPath("$.fireAlertStationDTO.[0].station", is(42)));
+    }
+
+
+    @Test
+    void getFloodTest() throws Exception {
+
+        List<FloodPersonsDTO> floodPersonsDTOS = new ArrayList<>();
+        FloodPersonsDTO floodPersonsDTO = new FloodPersonsDTO();
+        floodPersonsDTO.setLastName("Dupon");
+        floodPersonsDTO.setAge(16);
+        floodPersonsDTOS.add(floodPersonsDTO);
+
+        FloodDTO floodDTO = new FloodDTO();
+        floodDTO.setAddress("TestAddress");
+        floodDTO.setFloodAlertPersonsDTO(floodPersonsDTOS);
+
+        when(floodAlertService.getFloodDTO(int)).thenReturn(floodDTO);
+
+        mockMvc.perform(get("/flood/stations?stations=42"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.address", is("TestAddress")))
+                .andExpect(jsonPath("$.floodAlertPersonsDTO.[0].lastName", is("Dupont")));
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- Warning/src/test/java/com/mg/warning/person/PersonControllerTest.java
+++ Warning/src/test/java/com/mg/warning/person/PersonControllerTest.java
@@ -1,85 +1,106 @@
-//package com.mg.warning.person;
-//
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.mg.warning.controller.PersonController;
-//import com.mg.warning.model.Person;
-//import com.mg.warning.repository.PersonRepository;
-//import org.junit.jupiter.api.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-//import org.springframework.boot.test.context.TestConfiguration;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.context.junit4.SpringRunner;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-//
-//@RunWith(SpringRunner.class)
-//@WebMvcTest(controllers = PersonController.class)
-//class PersonControllerTest {
-//
-//    @TestConfiguration
-//    static class AdditionalConfig {
-//        @Bean
-//        public PersonRepository personRepository() {
-//            return new PersonRepository();
-//        }
-//    }
-//
-//    @Autowired
-//    private MockMvc mvc;
-//
-//    @Test
-//    public void testGetPerson() throws Exception {
-//        mvc.perform(MockMvcRequestBuilders.get("/person/GetAll"))
-//                .andExpect(status().isOk());
-//    }
-//
-//    @Test
-//    public void testPostPerson() throws Exception {
-//        mvc.perform(MockMvcRequestBuilders
-//                        .post("/person")
-//                        .content(asJsonString(new Person("TestFirstName", "TestLastName", "TestAddressTOTO", "", 0, "", "")))
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .accept(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk());
-//    }
-//
-//    @Test
-//    public void testPutPerson() throws Exception {
-//        mvc.perform(MockMvcRequestBuilders
-//                .post("/person")
-//                .content(asJsonString(new Person("TestFirstName", "TestLastName", "TestAddressTOTO", "", 0, "", "")))
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .accept(MediaType.APPLICATION_JSON));
-//
-//        mvc.perform(MockMvcRequestBuilders
-//                        .put("/person")
-//                        .content(asJsonString(new Person("TestFirstName", "TestLastName", "TestAddressDADA", "", 0, "", "")))
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .accept(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk());
-//    }
-//
-//    @Test
-//    public void testDeletePerson() throws Exception {
-//        {
-//            mvc.perform(MockMvcRequestBuilders
-//                            .delete("/person/TestFirstName/TestLastName")
-//                            .contentType(MediaType.APPLICATION_JSON))
-//            .andExpect(status().isOk());
-//        }
-//    }
-//
-//    public static String asJsonString(final Object obj) {
-//        try {
-//            return new ObjectMapper().writeValueAsString(obj);
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-//
-//}
+package com.mg.warning.person;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mg.warning.controller.PersonController;
+import com.mg.warning.model.Person;
+import com.mg.warning.service.PersonService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@WebMvcTest(PersonController.class)
+class PersonControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private PersonService PersonService;
+
+    private Person PersonTest1;
+    private Person PersonTest2;
+    private Person PersonTest3;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        PersonTest1 = new Person("TestFirstName1", "TestLastName1", "TestAddress1", "", 0,"","");
+        PersonTest2 = new Person("TestFirstName2", "TestLastName3", "TestAddress2", "", 0,"","");
+        PersonTest3 = new Person("TestFirstName2", "TestLastName3", "TestAddress2", "", 0,"","");
+    }
+
+    @Test
+    void getAllMedicalrecordsTest() throws Exception {
+
+        when(PersonService.findAll()).thenReturn(List.of(
+                PersonTest1, PersonTest2, PersonTest3));
+
+        mockMvc.perform(get("/person/GetAll"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(3)))
+                .andExpect(jsonPath("$[0].address", is("TestAddress1")))
+                .andExpect(jsonPath("$[1].address", is("TestAddress2")))
+                .andExpect(jsonPath("$[2].address", is("TestAddress2")));
+    }
+
+    @Test
+    void getAllFireStationsWhenNotFoundTest() throws Exception {
+
+        when(PersonService.findAll()).thenReturn(Collections.emptyList());
+
+        mockMvc.perform(get("/person/GetAll"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(0)));
+    }
+
+    @Test
+    void saveFireStationsTest() throws Exception {
+
+        mockMvc.perform(post("/person")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(new Person("TestFirstName1", "TestLastName1", "TestAddress1", "", 0,"","")))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testPutFirestation() throws Exception {
+
+        mockMvc.perform(put("/person")
+                        .content(asJsonString(new Person("TestFirstName1", "TestLastName1", "TestAddress1", "", 0,"","")))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testDeleteFirestation() throws Exception {
+        {
+            mockMvc.perform(delete("/person/TestFirstName/testLastName")
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk());
+        }
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
